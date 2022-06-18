237588c0d5f05e89ac92476dff3108734f42f23c
Halilhan İnan <88934162+halilhaninan@users.noreply.github.com>
2022-06-18 16:03:54 +0300

    Added my project

---
+++ src/Compenents/walletlistData.js
@@ -0,0 +1,253 @@
+const walletlistData = {
+  "0x6fc723b3a8228232F205AF109Fac813dA8E5220E": {
+    secretKey:
+      "0x7ab409d8b2e3258c983f4a21fbd51eaf9201bd90627984c2fb6b4835a375dc59",
+    nonce: 0,
+  },
+  "0xf776B9f81DC2DdAFff66D1a8E67679D1EC401b2e": {
+    secretKey:
+      "0x8f42d556fb9c220da8aab6a2fb8d616a09194ef736033ae5e74396983e941152",
+    nonce: 0,
+  },
+  "0x6edb8CA95E1Be647eEe72916754233Af62B477FC": {
+    secretKey:
+      "0xa9cfa2d521010249cc1a18b1ece0389ac10d72a438194d8a773126d8bd5ecb86",
+    nonce: 0,
+  },
+  "0x9cb4A9e81A7B9eD90Aa315F2C59009d972A3A335": {
+    secretKey:
+      "0xcae80458cf4bec9170f7fac7027ed03bd806c6929e2b331245c6729850c5f2e9",
+    nonce: 0,
+  },
+  "0x93e4ABf6b2fB2aBae8ac522EB57C9A9B23461988": {
+    secretKey:
+      "0xc67cbe5ff175b4da8cb15107c162a4333c3633be7799d613cf54c81807fe35e8",
+    nonce: 0,
+  },
+  "0x5F1751765a2515eff2f5Cbf568eEd74217430343": {
+    secretKey:
+      "0x334e723fd04aa81d58676a7a9a2edd62a27dec8d3c28e720ec044b4c0913340c",
+    nonce: 0,
+  },
+  "0x3bD62876f6a92ba73bf0e3a5D6Ad7b602AE65054": {
+    secretKey:
+      "0x8beead5ebb29102d8dab037903186120a11a7a983dfbd91066dca032673277b4",
+    nonce: 0,
+  },
+  "0x95d39e3294DbA45118C8a7aed338074aFb181465": {
+    secretKey:
+      "0x2e8a3b02846f4cc0b179a610e822ab7c7ee9e7cce2e6cb67a5326233a3afc3b0",
+    nonce: 0,
+  },
+  "0x2Fa2a032a3E1dE9Ea1b396313469f11879a91f9c": {
+    secretKey:
+      "0x30bef3f249d5b7602e69b74d69a6b955741b9d5102b0763ef3ebbaab0ddcb4d7",
+    nonce: 0,
+  },
+  "0xeEd156EdFDF07F9ECa39122C4AEAFC4B69c1fa65": {
+    secretKey:
+      "0x947b186789f4251de231f6673c6fc9cfe185b92dbf4df1aae6ab3ac1975adc0f",
+    nonce: 0,
+  },
+  "0xbe9BF8904C1e81239479C3e4a6f0843E8B949F45": {
+    secretKey:
+      "0xc75936173904e65a70614ff8fb997c88a402c8b3c6c8280ee4f72a3ed2ba8621",
+    nonce: 0,
+  },
+  "0x68A6904f915E74FC12eD156CE667142ccd180A29": {
+    secretKey:
+      "0xb969fbf17ecf3aafe498ac5fa19182b871b287d9ee27fccdfad5934e7db1c0c0",
+    nonce: 0,
+  },
+  "0x907046AbE238fde178213B6C54A4aFF3BC925096": {
+    secretKey:
+      "0x5dccef1f6c3952655280e36295efda5e97760656e66a411616147ef0968a0574",
+    nonce: 0,
+  },
+  "0x0327a06F3b96ebcF76Abbf016fa5F226c5e3e16a": {
+    secretKey:
+      "0x5de436a4c58347a8784bcd498d785eaeed10b3a2f1481017d962bdf3342e31ee",
+    nonce: 0,
+  },
+  "0xe4956BDE8BFd693F769FA2463E968E998e657236": {
+    secretKey:
+      "0x3e3caffc3fb8056c9f3b04f9cae5a534b6953756582eecfbbf0cc7092cb4d5c5",
+    nonce: 0,
+  },
+  "0xc6E361906e03a94e9b0b2a2162D58621D86Ec94c": {
+    secretKey:
+      "0x6d636a5b57dd61f33e5c4830f35e7a5263b00f441abf7821e8c5e5edcd0c012e",
+    nonce: 0,
+  },
+  "0x6f3E42b940b746050D22FFeD0ea291f25c9Daf3D": {
+    secretKey:
+      "0x3f9846150702af2027a7af090e99936e8a358e57eaf2580e586d751c3869335f",
+    nonce: 0,
+  },
+  "0x9F66B2767C9C55E29F3E70a145DcD8104C1F6e01": {
+    secretKey:
+      "0x8807924522f18fcd0cfa096794aec0a41b20b9127a009b87807eebc696e92cbe",
+    nonce: 0,
+  },
+  "0x1D3c71FA79F5f3Cc88bCcC81592efdB421Eef896": {
+    secretKey:
+      "0x4878c55119e25b16821fc0158e5b0e034e6e28f7d4a5c24b60c0d0717e31c064",
+    nonce: 0,
+  },
+  "0xCbDA959D502cbEaD0f590068c86AfD80379eb2B0": {
+    secretKey:
+      "0x959c899d749452fdab56bd3d4948f462d5875a4ff9a0ebcd5b337c9618ea593b",
+    nonce: 0,
+  },
+  "0xD476ecbA08FE848ad9CC45B7335c4C0B0b0786D9": {
+    secretKey:
+      "0xa9552c2104c83e53b5b8b46227c50cf5279205f333ae50ebea7def397af1c609",
+    nonce: 0,
+  },
+  "0x1ad2bFEab8BDCfaB4f3ec493dd5C4d1b1F274ADa": {
+    secretKey:
+      "0x9f43cfdaaa36feb3f8036975ffd959d7d4145993b5d309587204a64040c6486d",
+    nonce: 0,
+  },
+  "0x032E02B48889d2E97BFE010FC8722F507679aB37": {
+    secretKey:
+      "0x48d4eba8b434bf9dc075840c15e82979025c05b8b83a9ddee6a81a790399a521",
+    nonce: 0,
+  },
+  "0x10C2dF441B3dcCae7116455062292b2D85C06694": {
+    secretKey:
+      "0x9de32da6505240fe4a4f30ba44271193b6f2b26b9f9f31ef4ce46094840c735b",
+    nonce: 0,
+  },
+  "0x7a0871187fa844542995186C4b91c4aCc9a6a90c": {
+    secretKey:
+      "0x3837cb1f4a92b0980b3113a3a82355ed0f6619f77072ac09c58f455fcfa6184a",
+    nonce: 0,
+  },
+  "0xF2134f506065DF91537Ee61a393A71e5520203Db": {
+    secretKey:
+      "0x0fa2371db2a703db241c9af7ca40c93b124d88120de892b1e542cf1686bd5567",
+    nonce: 0,
+  },
+  "0x9586CD1D9F69255918ef481A65b691D7d68A5684": {
+    secretKey:
+      "0x7ae7623dac6cc8f305abb7731fea896058a9ee26a9e53874ce109c544af37494",
+    nonce: 0,
+  },
+  "0x63Aa3f9b1123e0dC73bA29af9A4E429e618b7dB8": {
+    secretKey:
+      "0xf17c4831ee39d32903866976f54a89cb47e54182b190b0ca7ecfe57753ffeeef",
+    nonce: 0,
+  },
+  "0x5FE78b9Fe8fd9e14d5d4bf9CAAD271f6e115f8B9": {
+    secretKey:
+      "0x89107024686f4da6c2f1aa4ee9ef02dd7d3e90c539b8132fa0444c564b6884ec",
+    nonce: 0,
+  },
+  "0x0DD9bFBF2Fb16519Eca7A0CA1a9E5968c4bF6f76": {
+    secretKey:
+      "0x9f8e04f6f8b3c6bbdfa8964c3bf882d38764a753281b841dc8bfaaba48322127",
+    nonce: 0,
+  },
+  "0xbA6BC68C8b5D3cf7c8C52C95a6324744b9387b8d": {
+    secretKey:
+      "0xf6b795fbb8c3daae71bd217a1b309fb5e1dd390632f4609b81cf97d4c3f5fe9f",
+    nonce: 0,
+  },
+  "0x871A8af9235d9D9487413405174D9FD54Be0e75A": {
+    secretKey:
+      "0x2cf1c2d138fe8195af7c48f027dc95cc3cc54d4c1213043a729f488e5fecffb3",
+    nonce: 0,
+  },
+  "0x8108f9AB983452733675Cf90Ec72536865d03537": {
+    secretKey:
+      "0x3966d2104d37f59ba7670f3f5140668af363b4d5f6cee89bae91ef0669ecc7c9",
+    nonce: 0,
+  },
+  "0xFb5597D55d92384f1C97A6A6EE494b58aE2cE8C7": {
+    secretKey:
+      "0xbee3169b05069ac99e24b3a8a3ffc5845f7c44b18547482b6c86d356d14871da",
+    nonce: 0,
+  },
+  "0x53db9cE0A4Ca73aEEc8BD0BBE720E4301eC23857": {
+    secretKey:
+      "0x669922bf55a67088c2e5fd00fcb1db1d81aac0a93662c227ed3aca88ddc934ae",
+    nonce: 0,
+  },
+  "0xc65A7A560C9FF14d710edAa7c4C14909eBa74348": {
+    secretKey:
+      "0x1fee2ac6cc7ec390d9663e1322cc79cb38bd718a43513991f8380ff3b2f4ee77",
+    nonce: 0,
+  },
+  "0x3D768eb02260DA0072561bAF87Eae886b1440239": {
+    secretKey:
+      "0x96b5ab93207686ab035227a83b53be375b020144bb31fb98dd4a6b26b4205267",
+    nonce: 0,
+  },
+  "0x0Cd8F83ae29e39AbdC40433CC031d4EFB36a1791": {
+    secretKey:
+      "0xdd16c79b48d3aa452cc1859a9fa56245e785415f7b78772e9195876496ea4086",
+    nonce: 0,
+  },
+  "0x9A836B27688a8baA7B78138A26Db6446B49034AC": {
+    secretKey:
+      "0x17fb4a03eb83f7d063bdca0a6124e6102270182a58f07c38eb3cb761b23cbb6f",
+    nonce: 0,
+  },
+  "0xfd99078a295a1EF36987CCf519B3bAE6393690C7": {
+    secretKey:
+      "0x08c9d9a8cc929c196b03de1615659f30bcb8e9b0c22af970a4f649f7f2cf3ef6",
+    nonce: 0,
+  },
+  "0x8be7d499555B40C6468c051982E8510eD06c8d1A": {
+    secretKey:
+      "0xbdf24e37a467a45b6077d458931883f0ada99a4e3a5a852ba1902c1f46162523",
+    nonce: 0,
+  },
+  "0x64E9b2cD0f75D81B2f26d7b441402B6f92aa8d2A": {
+    secretKey:
+      "0xc9732200053b9cc76015645faeac821aa19da716fee518444f928ba9fa2a4466",
+    nonce: 0,
+  },
+  "0x3878BFFe17701F3c7DaFc573cA6520f12ADF4578": {
+    secretKey:
+      "0xf0c79c7b67618fa718124fd7a4f51fede0a6c9d59d4abdb5c5e9058ef4c48edd",
+    nonce: 0,
+  },
+  "0x3Aa536674D66fe9D890c2E70ede5b92d2fCc9bd0": {
+    secretKey:
+      "0x0130f3e8e4731bc2441aaa0d005f42dd9c776b6e3202e8313451ef5a8722a21c",
+    nonce: 0,
+  },
+  "0x3AD4B4B2D8F53f261214ab9D1A3513D81802bfa4": {
+    secretKey:
+      "0x525e82ee7295e0a4239aabb20214e63811224070f3d4b3d59d8d558f7f6e9f9f",
+    nonce: 0,
+  },
+  "0x494c0dB4e00fDc56A02C2C65aa17d786A321876F": {
+    secretKey:
+      "0xb436e4902e5835ad4bcc86efb1d49fb8fc6ada2ca7570dd57b03cec2698fe312",
+    nonce: 0,
+  },
+  "0xfF2fe75E0c8cb793Daf3ab70186437669B0a97Ac": {
+    secretKey:
+      "0xacbc520fe19b96071b82e81b55bbd04d26b256c317dee8ebd23dfe4dd0c171ea",
+    nonce: 0,
+  },
+  "0xE01595d6396003615F7F9D8b7d5b961286b83a68": {
+    secretKey:
+      "0xb564848ea1f73674535c38145d48e74bb897ec60ec19725f7ee6f937d9eb69e4",
+    nonce: 0,
+  },
+  "0xf4730C680F37Bc2a762bB8da34e8710b08B7ed45": {
+    secretKey:
+      "0xe7036e0df847bc6cbe81e622c6e3fc52e31352e4e384a307443291b61e6573b7",
+    nonce: 0,
+  },
+  "0xAC0E26F73e63b40beAA111A9a4f872646e5be93E": {
+    secretKey:
+      "0x5c82f397894778c14cd54f13cd488fb56ba9e72a387449e1e9d86741e070dfa9",
+    nonce: 0,
+  },
+};
+export default walletlistData;
